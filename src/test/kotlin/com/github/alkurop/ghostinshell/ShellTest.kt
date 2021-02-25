667d420419696b23a106c554181cf8d96b331685
Alexey Kuropiatnyk <alexey.kuropiantnyk@ah.nl>
2021-02-25 21:33:55 +0100

    Add tests

--- src/test/kotlin/com/github/alkurop/ghostinshell/ShellTest.kt
+++ src/test/kotlin/com/github/alkurop/ghostinshell/ShellTest.kt
@@ -1,10 +1,28 @@
 package com.github.alkurop.ghostinshell
 
 import org.junit.Test
+import kotlin.test.assertEquals
 
-internal class ShellTest{
+internal class ShellTest {
+
+    private val testGhost = "ghost"
+
     @Test
-    fun helloGhost(){
-        val shell = Shell()
+    fun `first call to nonempty shell returns ghost`() {
+        val shell = Shell(testGhost)
+        assertEquals( testGhost, shell.ghost)
+    }
+
+    @Test
+    fun `second call to nonempty ghost returns null`() {
+        val shell = Shell(testGhost)
+        shell.ghost
+        assertEquals(null, shell.ghost)
+    }
+
+    @Test
+    fun `first call to empty shell returns null`() {
+        val shell = Shell.empty<String>()
+        assertEquals(null, shell.ghost)
     }
 }
