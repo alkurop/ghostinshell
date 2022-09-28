4dcb5cc9c2be2028156a637b2da737a92507016c
Alex Kuropiatnyk <allkurop@gmail.com>
2022-09-28 22:32:48 +0200

    Fix dep issue

--- src/main/kotlin/com/github/alkurop/ghostinshell/Shell.kt
+++ src/main/kotlin/com/github/alkurop/ghostinshell/Shell.kt
@@ -3,9 +3,8 @@ package com.github.alkurop.ghostinshell
 import java.util.concurrent.atomic.AtomicReference
 
 class Shell<T>(ghost: T?) {
-    val ghost: AtomicReference<T> = AtomicReference(ghost)
-
-    fun get(): T? = ghost.getAndSet(null)
+    private val refHolder: AtomicReference<T> = AtomicReference(ghost)
+    val ghost: T? = refHolder.getAndSet(null)
 
     companion object {
         fun <T> empty(): Shell<T> = Shell(null)
